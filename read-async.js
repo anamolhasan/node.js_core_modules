42c88c0dc01f97150b4b2a22c95ff79e3a3d6be6
anamolhasan <mdanamolhasan1998@gmail.com>
2025-11-22 16:25:50 +0600

    reading fileles

---
+++ read-async.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+console.log('start reading....')
+
+fs.readFile('./data/diary.txt', 'utf-8', (error, data ) => {
+    if(error){
+        console.error('error happend : ', error.message)
+    }
+    console.log('file count :')
+    console.log(data)
+})
+console.log('This runs immediately - no blocking')
---
+++ read-sync.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+try{
+    const data = fs.readFileSync('./data/diary.txt', 'utf-8')
+    console.log(data)
+}catch(err){
+    console.error(err.message)
+}
